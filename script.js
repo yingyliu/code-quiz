51d376a543b9c5761803e23de51cd479444eba3f
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-02 14:10:58 -0700

    add creatEl and setAtt in js

--- script.js
+++ script.js
@@ -18,80 +18,91 @@ function decrement (){
     }
 }
 
-// Introduction of Quiz
-var myIntroduction = [
-    {
-    heading: "Coding Quiz Challenge",
-    subtitle: "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by 5 seconds!"
-    }
-]
-    
 
+
+
+var main = document.main;// Introduction of Quiz
+var h1El = document.createElement("h1"); //Introduction of Quiz
+var h2El = document.createElement("h2"); //questions
+var h3El = document.createElement("h3"); //answers
+var listEl = document.createElement("div"); //create order list element
+//create order list items
+var liA = document.creatElement("li"); 
+var liB = document.creatElement("li"); 
+var liC = document.creatElement("li");
+var liD = document.creatElement("li");
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center; font-size:30px");
+subtitle.setAttribute("style", "margin:auto; width:50%; text-align:center; font-size:20px");
+//Add styleing to list elment
+liA.setAttribute("style", "background:purple; padding: 5px");
+liB.setAttribute("style", "background:purple; padding: 5px");
+liC.setAttribute("style", "background:purple; padding: 5px");
+liD.setAttribute("style", "background:purple; padding: 5px");
+
+console.log("Coding Quiz Challenge", main);
+h1El.textContent = "Coding Quiz Challenge";
+subtitle.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by 5 seconds!"; 
 
 startButton.addEventListener("click", startTimer);
 
 //quiz set up
 var quizContainer = document.querySelector("#quiz");
-var resultsContainer = document.querySelector("#results");
-var submitButton = document.querySelector("submit");
 
-var myQuestions = [
-    {
-        question:"Which of the following is an advantage of using JavScript?",
-        answers:{
-            a:"Less server interaction",
-            b:"Immediate feedback to the visitors",
-            c:"Increased interactivity",
-            d:"All of the above"
-        },
-        correctAnswer: "D"
-    },
-    {
-        question:"Which built-in method returns the calling string value converted to upper case?",
-        answers:{
-            a:"toUpperCase()",
-            b:"toUpper()",
-            c:"changeCase",
-            d:"None of the above"
-        },
-        correctAnswer: "A"
-    }
-    {
-        question:"Which of the following function of Array object returns a string representing the array and its elements?",
-        answers:{
-            a:"toSource()",
-            b:"toString",
-            c:"splice()",
-            d:"sort()"
-        },
-        correctAnswer: "B"
-    }
-    {
-        question:"What JavaScript keyword declares a variable?",
-        answers:{
-            a:"create",
-            b:"if",
-            c:"var",
-            d:"for"
-        },
-        correctAnswer: "C"
-    }
-    {
-        question:"What should appear at the very end of your JavaScript? \n The<script LANGUAGE=JavaScript>tag",
-        answers:{
-            a:"The <script>",
-            b:"The END statement",
-            c:"None of the above",
-            d:"The </script>"
-        },
-        correctAnswer: "D"
-    }
-]
+//Question 1
+h2El.textContent = "Q1. Which of the following is an advantage of using JavScript?";
+liA.textContent = "Less server interaction";
+liB.textContent = "Immediate feedback to the visitors";
+liC.textContent = "Increased interactivity";
+liD.textContent = "All of the above";
 
-//wrong answer with subtracted time from the clock
+correctAnswer = "D"; //h3
+wrongAnswer = "Wrong";//display text "wrong" subtracted timer with 5s.
+
+//Question 2
+h2El.textContent = "Q2. Which built-in method returns the calling string value converted to upper case?";
+liA.textContent = "toUpperCase()";
+liB.textContent = "toUpper()";
+liC.textContent = "changeCase";
+liD.textContent = "None of the above";
+
+correctAnswer = "A"; //h3
+wrongAnswer = "Wrong!";//display text "wrong" subtracted timer with 5s.
+
+//Question 3
+h2El.textContent = "Q3. Which of the following function of Array object returns a string representing the array and its elements?";
+liA.textContent = "toSource()";
+liB.textContent = "toString";
+liC.textContent = "splice()";
+liD.textContent = "sort()";
+
+correctAnswer = "B"; //h3
+wrongAnswer = "Wrong!";//display text "wrong" subtracted timer with 5s.
+
+//Question 4
+h2El.textContent = "Q4. What JavaScript keyword declares a variable?";
+liA.textContent = "create";
+liB.textContent = "if";
+liC.textContent = "var";
+liD.textContent = "for";
+
+correctAnswer = "C"; //h3
+wrongAnswer = "Wrong!";//display text "wrong" subtracted timer with 5s.
+
+//Question 5
+h2El.textContent = "Q5. What should appear at the very end of your JavaScript? \n The<script LANGUAGE=JavaScript>tag";
+liA.textContent = "The <script>";
+liB.textContent = "The END statement";
+liC.textContent = "None of the above";
+liD.textContent = "The </script>";
+
+correctAnswer = "D"; //h3
+wrongAnswer = "Wrong!";//display text "wrong" subtracted timer with 5s.
 
 
 //all questions are answered or the timer reaches 0, game is over
 
 
 //save initial name and score
+var resultsContainer = document.querySelector("#results");
+var submitButton = document.querySelector("submit");
