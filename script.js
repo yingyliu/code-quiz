b0b142c78776b4eede57b189af256bd34d007519
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-01 21:24:40 -0700

    adjust timer setting

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-//Setting countdown timer
-var number = 6;
-var intervalId;
+// timer set up when user click the start quiz button
+var startButton = document.querySelector("#start");
 
-document.getElementById("start").addEventListener("click", run);
+var totalSeconds = 5;
+var interval;
 
-function run(){
-    intervalId = setInterval(decrement, 1000);
-    if (number === 0) {
-        alert("Time Up!");
+function startTimer() {
+    interval = setInterval(decrement, 1000);
+}
+
+function decrement (){
+    totalSeconds--;
+    document.querySelector("#timer").innerHTML = "<h2>Time: " + totalSeconds + "</h2>";
+    
+    if (totalSeconds === 0) {
+        clearInterval(interval);
+        document.querySelector("#timer").innerText = "<h2>Game Over!</h2>";
     }
 }
 
-function decrement(){
-    number--;
-    document.getElementById("timer").innerHTML = "<h2>Time:" + number + "</h2>";
+startButton.addEventListener("click", startTimer);
 
-}
+//quiz set up
+
+
+//wrong answer with subtracted time from the clock
+
+
+//all questions are answered or the timer reaches 0, game is over
+
+
+//save initial name and score
